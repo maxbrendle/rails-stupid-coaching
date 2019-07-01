79f83f5385fa63d30eb8df429720cb65e4604dc3
Max Brendle <maxbrendle@icloud.com>
2019-07-01 15:23:46 +0200

    Not beatiful but works

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+
+  end
+
+  def answer
+    @questions = params[:question]
+    return @answer = 'Great!' if @questions == 'I am going to work'
+
+    if @questions.include?('?')
+      return @answer = 'Stupid question. Get dressed and go to work!'
+    end
+
+    @answer = "I don't care. Get dressed and go to work!"
+  end
+end
